2806a7b66fadae76d694f29f367785702cd12037
Ismail <cucevicismail@gmail.com>
2019-01-19 16:10:51 +0100

    save changes

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,3 +1,13 @@
-//Start up point for the client side application
 
-console.log('Hi there!');
+// start up point fot the client side application
+import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+
+ReactDOM.hydrate(
+      <BrowserRouter>
+         <Routes />
+      </BrowserRouter>    
+    , document.querySelector('#root')
+    );
